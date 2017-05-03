c63602668672569e176561f972122987850008e5
Dr Switch <paykis@mail.ru>
2017-05-03 14:58:24 +0800

    Каркас проекта

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,7 +10,7 @@ public class Main extends Application {
      @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("FORMS/MainForm.fxml"));
         primaryStage.setTitle("Hello world");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 600, 475));
--- src/main/java/controllers/MainFormController.java
+++ src/main/java/controllers/MainFormController.java
@@ -1,21 +1,30 @@
-package sample;
-import bdconnection.StudentServiceImpl;
-import bdconnection.CityServiceImpl;
+package controllers;
+import dbconnection.StudentServiceImpl;
+import dbconnection.CityServiceImpl;
 
 import entity.CityEntity;
 import entity.StudentEntity;
+
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -129,8 +138,20 @@ public class MainFormController  implements Initializable {
     public  void RegionbtnClick(){
 
     }
-    public  void CitybtnClick(){
 
+    public  void CitybtnClick(ActionEvent actionEvent) throws Exception{
+        try {
+            Stage stage = new Stage();
 
+            Parent parent = FXMLLoader.load(getClass().getClassLoader().getResource("FORMS/CityForm.fxml"));
+            stage.setTitle("Страна");
+            stage.setScene(new Scene(parent, 430, 400));
+            stage.setResizable(false);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
